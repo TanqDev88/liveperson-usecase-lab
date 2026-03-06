d1f1db44e03fddf059820214b912a18e9e7b05b2
TanqDev88 <oliverprim783@gmail.com>
2026-03-05 23:53:19 -0300

    feat: integration function is added

--- lp-global-functions-lab/uc/deste/index.js
+++ lp-global-functions-lab/uc/deste/index.js
@@ -1,7 +1,13 @@
 var WHITELIST_PHONES_OPERADOR_DESVINCULACION = [
-    '5491159575879',
-    '5491130000002'
+    '5491159575878',
+    '5491170355625',
+    '5491158396301',
+    '5491156262039',
+    '5491123449456',
+    '5492396487226',
+    '5491157281705'
 ];
+
 function isWhiteListOperadorDesvinculacion() {
     var res = false;
     for (var e in WHITELIST_PHONES_OPERADOR_DESVINCULACION) {
@@ -11,14 +17,20 @@ function isWhiteListOperadorDesvinculacion() {
     }
     return res;
 }
-function T_Intro__DESTE_preProcessMessage() {
 
+function T_Intro__DESTE_preProcessMessage() {
+    resetVars_DESTE();
     if (!isWhiteListOperadorDesvinculacion()) {
-        goNext('T_Error en desvinculacion @DESTE');//Esta interaccion esta incorrecta solo es para probar el listado de nuemeros
+        goNext('T_Funcionalidad apagada @DESTE');
         return;
     }
-    goNext('Q_Solicitud_DNI__DESTE');
+
+    goNext('Q_Solicitud DNI y genero @DESTE');
 }
+
+//**                                            MANEJO DNI + GENERO                                                                      **
+
+
 function obtenerDniYGeneroDesdeString(input) {
     if (!input || typeof input !== 'string') {
         return null;
@@ -37,6 +49,45 @@ function obtenerDniYGeneroDesdeString(input) {
         genero: match[2].toUpperCase()
     };
 }
+
+function api_integration_desvincularNumeroTercero_DESTE_postProcessMessage() {
+    try {
+        var httpCode = Number(get_universalStatusCode());
+        if (isApiExecutionFail()) {
+            logDebug('FALLO LA INTEGRACION Desvinculacion tercero');
+            logDebug('HTTP CODE:', Number(httpCode));
+            switch (httpCode) {
+                case 409:
+                    logDebug('RESPUESTA 409 - No se encuentra vinculación con el DNI proporcionado.');
+                    goNext('T_Cliente sin numero telefonico @DESTE');
+                    break;
+
+                case 401:
+                    logDebug('ERROR 401');
+                    goNext('T_Error en desvinculacion @DESTE');
+                    break;
+
+                case 500:
+                    logDebug('ERROR 500 - Falla técnica');
+                    goNext('T_Error en desvinculacion @DESTE');
+                    break;
+
+                default:
+                    logDebug('ERROR inesperado');
+                    goNext('T_Error en desvinculacion @DESTE');
+            }
+
+        } else {
+            logDebug('SE DESVINCULÓ EXITOSAMENTE, código:', httpCode);
+            goNext('T_Desvinculacion exitosa @DESTE');
+        }
+
+    } catch (error) {
+        logDebug('Error inesperado', error);
+        goNext('T_Error en desvinculacion @DESTE');
+    }
+}
+
 function setDatosPersonales_DESTE() {
     var data = getBotVar('dataTercero');
 
